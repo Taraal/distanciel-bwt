27cac575fdba4032914901913fc928da39999968
Sylouan CORFA <corfa.sylouan@blacktiger.tech>
2021-12-01 19:37:05 +0100

    Deleted useless bwt

--- fm_index.py
+++ fm_index.py
@@ -5,18 +5,6 @@ def suffixArray(s):
     satups = sorted([(s[i:], i) for i in range(len(s))])
     return list(map(lambda x: x[1], satups)) 
 
-def bwtFromSa(t, sa=None):
-    bw = []
-    dollarRow = None
-    if sa is None:
-        sa = suffixArray(t)
-    for si in sa:
-        if si == 0:
-            dollarRow = len(bw)
-            bw.append('$')
-        else:
-            bw.append(t[si-1])
-    return ''.join(bw), dollarRow 
 
 class FmCheckpoints(object):
 
